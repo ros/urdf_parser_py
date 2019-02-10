7de64e3cc02bf1a916158941e1482e93676617b7
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2019-02-10 17:49:58 +0100

    Update setup.py to meet ROS 2 requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,41 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'urdfdom_py'
 
-d = generate_distutils_setup(
+setup(
+    name=package_name,
+    version='0.3.3',
+    package_dir={'': 'src'},
     packages=['urdf_parser_py', 'urdf_parser_py.xml_reflection'],
-    package_dir={'': 'src'}
+    data_files=[
+        ('scripts/display_urdf',
+        ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Víctor Mayoral Vilches',
+    author_email='vmayoral@acutronicrobotics.com',
+    maintainer='Víctor Mayoral Vilches',
+    maintainer_email='vmayoral@acutronicrobotics.com',
+    keywords=['ROS2'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Python implementation of the URDF parser.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
 )
 
-setup(**d)
+# from distutils.core import setup
+# from catkin_pkg.python_setup import generate_distutils_setup
+#
+# d = generate_distutils_setup(
+#     packages=['urdf_parser_py', 'urdf_parser_py.xml_reflection'],
+#     package_dir={'': 'src'}
+# )
+#
+# setup(**d)
+#
