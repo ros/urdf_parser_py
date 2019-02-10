1f72ca1c095501c9597d114faedd83086094d47b
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2019-02-10 17:36:49 +0100

    Port package.xml and setup.py to ROS 2

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from glob import glob
+from setuptools import setup
 
-d = generate_distutils_setup(
+package_name = 'urdfdom_py'
+
+setup(
+    name=package_name,
+    version='0.4.1',
+    package_dir={'': 'src'},
     packages=['urdf_parser_py', 'urdf_parser_py.xml_reflection'],
-    package_dir={'': 'src'}
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/scripts', glob('scripts/display_urdf')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    keywords=['ROS2'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Python implementation of the URDF parser.',
+    license='BSD',
+    tests_require=['pytest'],
 )
-
-setup(**d)
