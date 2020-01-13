261fb77f5829aa78c1cd47b662bb33f16494a082
Chris Lalancette <clalancette@openrobotics.org>
2020-01-13 16:13:26 -0500

    Add in support for the version tag. (#52)
    * Add in support for the version tag.
    
    Cherry-picked from the melodic-devel branch commit
    c0a75a2dd40c4b7a4fffdfe9776f9ec229609232
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- src/urdf_parser_py/urdf.py
+++ src/urdf_parser_py/urdf.py
@@ -533,6 +533,23 @@ class Robot(xmlr.Object):
         assert root is not None, "No roots detected, invalid URDF."
         return root
 
+    def post_read_xml(self):
+        if self.version is None:
+            self.version = "1.0"
+
+        split = self.version.split(".")
+        if len(split) != 2:
+            raise ValueError("The version attribute should be in the form 'x.y'")
+
+        if split[0] == '' or split[1] == '':
+            raise ValueError("Empty major or minor number is not allowed")
+
+        if int(split[0]) < 0 or int(split[1]) < 0:
+            raise ValueError("Version number must be positive")
+
+        if self.version != "1.0":
+            raise ValueError("Invalid version; only 1.0 is supported")
+
     @classmethod
     def from_parameter_server(cls, key='robot_description'):
         """
@@ -547,7 +564,8 @@ class Robot(xmlr.Object):
 
 
 xmlr.reflect(Robot, tag='robot', params=[
-    xmlr.Attribute('name', str, False),  # Is 'name' a required attribute?
+    xmlr.Attribute('name', str),
+    xmlr.Attribute('version', str, False),
     xmlr.AggregateElement('link', Link),
     xmlr.AggregateElement('joint', Joint),
     xmlr.AggregateElement('gazebo', xmlr.RawType()),
