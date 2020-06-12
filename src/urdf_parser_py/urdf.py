c62668574fe290398010b275115fd77da196edd5
Chris Lalancette <clalancette@openrobotics.org>
2020-06-12 16:51:13 +0000

    Make sure to add the version when creating a new URDF.
    It turns out that when I added the version stuff, I completely
    forgot about creating a new URDF.  This PR fixes it so that
    we can successfully round-trip through the URDF parser, and
    adds tests to ensure the same.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- src/urdf_parser_py/urdf.py
+++ src/urdf_parser_py/urdf.py
@@ -471,10 +471,16 @@ xmlr.add_type('transmission',
 
 
 class Robot(xmlr.Object):
-    def __init__(self, name=None):
+    SUPPORTED_VERSIONS = ["1.0"]
+
+    def __init__(self, name=None, version="1.0"):
         self.aggregate_init()
 
         self.name = name
+        if version not in self.SUPPORTED_VERSIONS:
+            raise ValueError("Invalid version; only %s is supported" % (','.join(self.SUPPORTED_VERSIONS)))
+
+        self.version = version
         self.joints = []
         self.links = []
         self.materials = []
@@ -547,8 +553,8 @@ class Robot(xmlr.Object):
         if int(split[0]) < 0 or int(split[1]) < 0:
             raise ValueError("Version number must be positive")
 
-        if self.version != "1.0":
-            raise ValueError("Invalid version; only 1.0 is supported")
+        if self.version not in self.SUPPORTED_VERSIONS:
+            raise ValueError("Invalid version; only %s is supported" % (','.join(self.SUPPORTED_VERSIONS)))
 
     @classmethod
     def from_parameter_server(cls, key='robot_description'):
