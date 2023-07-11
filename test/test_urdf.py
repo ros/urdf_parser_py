d4d94d22749ad5bede456cbbdb7c60cd5e689743
Adrian Zwiener <a.zwiener@rethinkrobotics.com>
2023-07-11 09:36:49 +0200

    Remove test for visual with name
    The Visual class does not have an attribute name anymore. Consequently,
    we can not expect the test to work.

--- test/test_urdf.py
+++ test/test_urdf.py
@@ -244,20 +244,6 @@ class TestURDFParser(unittest.TestCase):
         robot.add_link(link)
         self.xml_and_compare(robot, xml)
 
-    def test_visual_with_name(self):
-        xml = '''<?xml version="1.0"?>
-<robot name="test" version="1.0">
-  <link name="link">
-    <visual name="alice">
-      <geometry>
-        <cylinder length="1" radius="1"/>
-      </geometry>
-      <material name="mat"/>
-    </visual>
-  </link>
-</robot>'''
-        self.parse_and_compare(xml)
-
     def test_link_multiple_collision(self):
         xml = '''<?xml version="1.0"?>
 <robot name="test" version="1.0">
