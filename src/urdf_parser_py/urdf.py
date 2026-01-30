85f3a3f505b098fc52f8235feec3c2309c4a01bd
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2026-01-30 17:41:52 +0100

    make sure that new limits work with 1.2

--- src/urdf_parser_py/urdf.py
+++ src/urdf_parser_py/urdf.py
@@ -563,6 +563,26 @@ class Robot(xmlr.Object):
         if self.version not in self.SUPPORTED_VERSIONS:
             raise ValueError("Invalid version; only %s is supported" % (','.join(self.SUPPORTED_VERSIONS)))
 
+        # Validate that acceleration, deceleration, and jerk are only used in version 1.2+
+        major = int(split[0])
+        minor = int(split[1])
+
+        if major == 1 and minor < 2:
+            for joint in self.joints:
+                if joint.limit is not None:
+                    if joint.limit.acceleration is not None and joint.limit.acceleration != float('inf'):
+                        raise ValueError(
+                            "Joint '%s': acceleration attribute requires URDF version 1.2, "
+                            "but version %s was specified" % (joint.name, self.version))
+                    if joint.limit.deceleration is not None and joint.limit.deceleration != float('inf'):
+                        raise ValueError(
+                            "Joint '%s': deceleration attribute requires URDF version 1.2, "
+                            "but version %s was specified" % (joint.name, self.version))
+                    if joint.limit.jerk is not None and joint.limit.jerk != float('inf'):
+                        raise ValueError(
+                            "Joint '%s': jerk attribute requires URDF version 1.2, "
+                            "but version %s was specified" % (joint.name, self.version))
+
 
 xmlr.reflect(Robot, tag='robot', params=[
     xmlr.Attribute('name', str),
